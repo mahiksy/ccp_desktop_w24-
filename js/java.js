5adebb77e8cb5c7ada16bc40a4a6874bdfd2f08d
Mohammed Tajalli <97805518+mahiksy@users.noreply.github.com>
2024-04-07 12:44:19 -0400

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,18 @@
+var pages=["index","pets", "travel","recipes"]
+
+document.getElementById("#index").addEventListener("click", function() {
+        // Navigate to another HTML page
+        window.location.href = "index.html";
+});
+document.getElementById("#pets").addEventListener("click", function() {
+    // Navigate to another HTML page
+    window.location.href = "pets.html";
+});
+document.getElementById("#travel").addEventListener("click", function() {
+    // Navigate to another HTML page
+    window.location.href = "travel.html";
+});
+document.getElementById("#recipes").addEventListener("click", function() {
+    // Navigate to another HTML page
+    window.location.href = "recipes.html";
+});
